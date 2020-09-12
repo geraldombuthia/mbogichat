ee0b8ad35e07231474492062f3bfa85a3ea72d3a
Gerald Mbuthia <geraldombuthia@gmail.com>
2020-09-12 10:47:06 +0300

    removed turn Servers

--- public/script.js
+++ public/script.js
@@ -7,41 +7,7 @@ const myPeer = new Peer(undefined, {
   host: "/",
   port: "443",
   secure: true,
-  //   config: {
-  //     'iceServers': [
-  //         { url: 'stun:stun1.l.google.com:19302' },
-  //         {
-  //             url: 'turn:numb.viagenie.ca',
-  //             credential: 'muazkh',
-  //             username: 'webrtc@live.com'
-  //         }
-  //     ]
-  // }
-  config: {
-    iceServers: [
-      { url: "stun:stun01.sipphone.com" },
-      { url: "stun:stun.ekiga.net" },
-      { url: "stun:stunserver.org" },
-      { url: "stun:stun.softjoys.com" },
-      { url: "stun:stun.voiparound.com" },
-      { url: "stun:stun.voipbuster.com" },
-      { url: "stun:stun.voipstunt.com" },
-      { url: "stun:stun.voxgratia.org" },
-      { url: "stun:stun.xten.com" },
-      {
-        url: "turn:192.158.29.39:3478?transport=udp",
-        credential: "JZEOEt2V3Qb0y27GRntt2u2PAYA=",
-        username: "28224511:1379330808",
-      },
-      {
-        url: "turn:192.158.29.39:3478?transport=tcp",
-        credential: "JZEOEt2V3Qb0y27GRntt2u2PAYA=",
-        username: "28224511:1379330808",
-      },
-    ],
-  },
-
-  debug: 3,
+  debug: 3
 });
 
 const myVideo = document.createElement("video");
