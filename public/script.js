fe11f17bc3d7e2bf0209e2a377307db0942ff10e
Gerald Mbuthia <geraldombuthia@gmail.com>
2020-09-12 10:34:46 +0300

    added new turn servers

--- public/script.js
+++ public/script.js
@@ -3,70 +3,98 @@ const Peer = require("peerjs");
 const socket = io("/");
 const videoGrid = document.getElementById("video-grid");
 const myPeer = new Peer(undefined, {
-  path:"/peerjs",
+  path: "/peerjs",
   host: "/",
   port: "443",
   secure: true,
+  //   config: {
+  //     'iceServers': [
+  //         { url: 'stun:stun1.l.google.com:19302' },
+  //         {
+  //             url: 'turn:numb.viagenie.ca',
+  //             credential: 'muazkh',
+  //             username: 'webrtc@live.com'
+  //         }
+  //     ]
+  // }
   config: {
-    'iceServers': [
-        { url: 'stun:stun1.l.google.com:19302' },
-        {
-            url: 'turn:numb.viagenie.ca',
-            credential: 'muazkh',
-            username: 'webrtc@live.com'
-        }
-    ]
-}
+    iceServers: [
+      { url: "stun:stun01.sipphone.com" },
+      { url: "stun:stun.ekiga.net" },
+      { url: "stun:stunserver.org" },
+      { url: "stun:stun.softjoys.com" },
+      { url: "stun:stun.voiparound.com" },
+      { url: "stun:stun.voipbuster.com" },
+      { url: "stun:stun.voipstunt.com" },
+      { url: "stun:stun.voxgratia.org" },
+      { url: "stun:stun.xten.com" },
+      {
+        url: "turn:192.158.29.39:3478?transport=udp",
+        credential: "JZEOEt2V3Qb0y27GRntt2u2PAYA=",
+        username: "28224511:1379330808",
+      },
+      {
+        url: "turn:192.158.29.39:3478?transport=tcp",
+        credential: "JZEOEt2V3Qb0y27GRntt2u2PAYA=",
+        username: "28224511:1379330808",
+      },
+    ],
+  },
+
+  debug: 3,
 });
 
-
-const myVideo = document.createElement('video')
-myVideo.muted = true
-const peers = {}
-navigator.mediaDevices.getUserMedia({
-  video: true,
-  audio: true
-}).then(stream => {
-  addVideoStream(myVideo, stream)
-
-  myPeer.on('call', call => {
-    call.answer(stream)
-    const video = document.createElement('video')
-    call.on('stream', userVideoStream => {
-      addVideoStream(video, userVideoStream)
-    })
+const myVideo = document.createElement("video");
+myVideo.muted = true;
+const peers = {};
+navigator.mediaDevices
+  .getUserMedia({
+    video: true,
+    audio: true,
   })
+  .then((stream) => {
+    addVideoStream(myVideo, stream);
+    console.log("done videoStream");
+    myPeer.on("call", (call) => {
+      call.answer(stream);
+      const video = document.createElement("video");
+      call.on("stream", (userVideoStream) => {
+        addVideoStream(video, userVideoStream);
+      });
+    });
 
-  socket.on('user-connected', userId => {
-    connectToNewUser(userId, stream)
-  })
-})
+    socket.on("user-connected", (userId) => {
+      connectToNewUser(userId, stream);
+      console.log("userconnected");
+    });
+  });
 
-socket.on('user-disconnected', userId => {
-  if (peers[userId]) peers[userId].close()
-})
+socket.on("user-disconnected", (userId) => {
+  if (peers[userId]) peers[userId].close();
+  console.log("disconnected");
+});
 
-myPeer.on('open', id => {
-  socket.emit('join-room', ROOM_ID, id)
-})
+myPeer.on("open", (id) => {
+  socket.emit("join-room", ROOM_ID, id);
+});
 
 function connectToNewUser(userId, stream) {
-  const call = myPeer.call(userId, stream)
-  const video = document.createElement('video')
-  call.on('stream', userVideoStream => {
-    addVideoStream(video, userVideoStream)
-  })
-  call.on('close', () => {
-    video.remove()
-  })
+  const call = myPeer.call(userId, stream);
+  const video = document.createElement("video");
+  call.on("stream", (userVideoStream) => {
+    addVideoStream(video, userVideoStream);
+  });
+  call.on("close", () => {
+    video.remove();
+  });
 
-  peers[userId] = call
+  peers[userId] = call;
 }
 
 function addVideoStream(video, stream) {
-  video.srcObject = stream
-  video.addEventListener('loadedmetadata', () => {
-    video.play()
-  })
-  videoGrid.append(video)
+  video.srcObject = stream;
+  video.addEventListener("loadedmetadata", () => {
+    video.play();
+  });
+  videoGrid.append(video);
 }
